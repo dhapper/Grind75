7c52e77a3b98efac7d5da7bedc31c9050a4d2777
dhapper <dhaarshan.p@gmail.com>
2025-07-14 12:39:39 -0400

    first commit

---
+++ Java/TwoSum.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+    	
+        HashMap <Integer, Integer> map = new HashMap<>();	// number -> index
+        
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        
+        }
+        return null;
+    }
+}
