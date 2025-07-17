95315320d3aa56180b780a68dd3e676472c46e6c
dhapper <dhaarshan.p@gmail.com>
2025-07-17 18:59:32 -0400

    Adding Solution - Best Time to Buy Stock

---
+++ Java/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+
+        int minVal = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if (prices[i] < minVal){
+                minVal = prices[i];
+            } else if(prices[i] - minVal > maxProfit) {     // else means that even it last val is minVal, no calcs will be used
+                maxProfit = prices[i] - minVal;
+            }
+        }
+
+        return maxProfit;
+    }
+}
