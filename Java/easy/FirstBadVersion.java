eb1015a2c5ad4f83325e5a05b3fd239dd3c29a30
dhapper <dhaarshan.p@gmail.com>
2025-07-26 23:28:34 -0400

    Added Solution - First Bad Version

---
+++ Java/easy/FirstBadVersion.java
@@ -0,0 +1,23 @@
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (true) {
+            int mid = low + (high - low) / 2;
+            System.out.println(mid);
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid - 1))
+                    return mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+
+    // dummy method to remove errors
+    public boolean isBadVersion(int n) {
+        return true;
+    }
+
+}
